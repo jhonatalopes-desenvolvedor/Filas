93e62d999180feab601c5083641ae1a6e3b0df60
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-08-15 19:37:37 -0300

    Upgrade Project

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
-window.axios = axios;
+
+window.axios = axios.create();
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import './echo';
